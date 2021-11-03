b5c93e2c58d53355fb4048ecce7c9850a66fa16e
Shubh Joshi <shubhjoshi80@gmail.com>
2021-11-03 23:30:44 +0530

    update news app

--- news.go
+++ news.go
@@ -1,23 +1,30 @@
 package main
+
 import (
-    "encoding/json"
-    "fmt"
-    "image/color"
-    "io/ioutil"
-    "net/http"
-    "fyne.io/fyne/v2"
-    // "fyne.io/fyne/v2/app"
-    "fyne.io/fyne/v2/canvas"
-    "fyne.io/fyne/v2/container"
-    "fyne.io/fyne/v2/widget"
+	"encoding/json"
+	"fmt"
+
+	// "image/color"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+
+	"fyne.io/fyne/v2"
+
+	// "fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/canvas"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/layout"
+	"fyne.io/fyne/v2/widget"
 )
 var num_article int = 1
 var news News
+var news_url *url.URL
 func showNews() {
     // a := app.NewWithID("")
     w := myapp.NewWindow("SSJ News App")
-    w.Resize(fyne.NewSize(300, 400))
-    URL := fmt.Sprintf("https://gnews.io/api/v4/top-headlines?token=f4242aaddbb109f07654431e207e2412&lang=en&max=100")
+    w.Resize(fyne.NewSize(400,200))
+    URL := "https://gnews.io/api/v4/top-headlines?token=f4242aaddbb109f07654431e207e2412&lang=en&max=100"    
     //API
     res, _ := http.Get(URL)
     defer res.Body.Close()
@@ -25,37 +32,48 @@ func showNews() {
     news, _ = UnmarshalNews(body)
     fmt.Println(news)
     // num_article = int(news.TotalArticles)
-    label2 := widget.NewLabel(fmt.Sprintf("No of articles:%d",
-        news.TotalArticles))
+    
     //show title
-    label3 := widget.NewLabel(fmt.Sprintf("%s", news.Articles[1].Title))
+    label3 := widget.NewLabel(news.Articles[1].Title)
     label3.TextStyle = fyne.TextStyle{Bold: true}
     label3.Wrapping = fyne.TextWrapBreak
     // show articles
-    entry1 := widget.NewLabel(fmt.Sprintf("%s",news.Articles[1].Description))
+    entry1 := widget.NewLabel(news.Articles[1].Description)
     //entry1.MultiLine = true
     entry1.Wrapping = fyne.TextWrapBreak
     //entry1.Resize(fyne.NewSize(300, 400))
     //entry1.Resize(fyne.NewSize(300, 300))
     // label4 := widget.NewLabel(fmt.Sprintf("Article: %s", news.Articles[0].Description))
-    btn := widget.NewButton("Next", func() {
+    
+    news_url, _ = url.Parse(news.Articles[1].URL)
+    new_link := widget.NewHyperlink("",news_url)
+    next_icon := canvas.NewImageFromFile("static\\next-icon.jpg")
+    link_icon := canvas.NewImageFromFile("static\\link.jpg")
+    img := canvas.NewImageFromFile("static\\news2.png")
+    img.FillMode = canvas.ImageFillOriginal
+    img_container := container.NewGridWrap(fyne.NewSize(400,250), container.NewPadded(img))
+    btn := widget.NewButton("", func() {
         num_article += 1
         label3.Text = news.Articles[num_article].Title
         entry1.Text = news.Articles[num_article].Description
+        news_url,_ = url.Parse(news.Articles[num_article].URL)
+        new_link.URL = news_url
+        new_link.Refresh()
         label3.Refresh()
         entry1.Refresh()
     })
-    label1 := canvas.NewText("-- News App --", color.White)
-    label1.Alignment = fyne.TextAlignCenter
-    label1.TextStyle = fyne.TextStyle{Bold: true}
-    img := canvas.NewImageFromFile("static\\news1.png")
-    // img.Resize(fyne.NewSize(150, 150))
-    img.FillMode = canvas.ImageFillOriginal
-    e := container.NewVBox(label1, label3, entry1, btn)
+    x :=container.New(layout.NewVBoxLayout(),label3,layout.NewSpacer(), entry1,layout.NewSpacer(),
+        container.New(layout.NewHBoxLayout(),
+        container.NewGridWrap(fyne.NewSize(25,25), container.NewPadded(new_link, link_icon)),
+        layout.NewSpacer(),
+        container.NewGridWrap(fyne.NewSize(25,25), container.NewPadded(btn, next_icon)),
+        ),    
+    )
+
 	r, _ := fyne.LoadResourceFromPath("static\\newslogo.png")
 	w.SetIcon(r)
     // e.Resize(fyne.NewSize(300, 300))
-    w.SetContent(container.NewBorder(img, label2, nil, nil, e))
+    w.SetContent(container.NewBorder(img_container, nil, nil, nil, x))
     w.CenterOnScreen()
     w.Show()
 }
